355bd7cb2d32abe4efb3009efc25268fd03058c3
Meraj Khan <mmuk2002@gmail.com>
2024-02-15 19:13:36 -0600

    Hi

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,21 +1,31 @@
 // server/controllers/userController.js
+const User = require('../models/User');
 
 // Get a user's profile
 exports.getUser = (req, res) => {
-  res.send('User profile data');
+  User.findById(req.params.id)
+    .then(user => res.json(user))
+    .catch(err => res.status(400).json('Error: ' + err));
 };
 
 // Create a new user
 exports.createUser = (req, res) => {
-  res.send('User created');
+  const newUser = new User(req.body);
+  newUser.save()
+    .then(() => res.json('User created'))
+    .catch(err => res.status(400).json('Error: ' + err));
 };
 
 // Update a user
 exports.updateUser = (req, res) => {
-  res.send('User updated');
+  User.findByIdAndUpdate(req.params.id, req.body)
+    .then(() => res.json('User updated'))
+    .catch(err => res.status(400).json('Error: ' + err));
 };
 
 // Delete a user
 exports.deleteUser = (req, res) => {
-  res.send('User deleted');
+  User.findByIdAndDelete(req.params.id)
+    .then(() => res.json('User deleted'))
+    .catch(err => res.status(400).json('Error: ' + err));
 };
