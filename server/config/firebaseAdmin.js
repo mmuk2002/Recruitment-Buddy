04f0fe8d61ee902c85b2697c15679681fb32e81c
Meraj Khan <mmuk2002@gmail.com>
2024-04-02 01:31:02 -0500

    Reverting

--- server/config/firebaseAdmin.js
+++ server/config/firebaseAdmin.js
@@ -2,7 +2,7 @@
 
 const admin = require('firebase-admin');
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_CREDENTIALS);
+const serviceAccount = require('./serviceAccountCredentials.json');
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
