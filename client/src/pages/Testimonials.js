d147a8db491f5469e586bc504d3f9aa668a7cc2e
Elana Agarwal <elana.agarwal@gmail.com>
2024-04-13 20:58:04 -0500

    Testimonials

--- client/src/pages/Testimonials.js
+++ client/src/pages/Testimonials.js
@@ -23,14 +23,14 @@ const Testimonials = () => {
               borderColor: 'neutral.main',
               borderWidth: 1,
               borderStyle: 'solid',
-              color: 'text.primary',
+              color: '',
             }}>
               <Typography variant="body1" sx={{ flexGrow: 1, mb: 2 }}>{testimonial.text}</Typography>
               <Box display="flex" alignItems="center">
                 <Avatar src={testimonial.image} alt={testimonial.user} sx={{ width: 48, height: 48, mr: 2, border: 1, borderColor: 'neutral.light' }} />
                 <Box>
                   <Typography variant="subtitle1">{testimonial.user}</Typography>
-                  <Typography variant="body2" sx={{ fontStyle: 'italic', color: 'text.secondary' }}>
+                  <Typography variant="body2" sx={{ fontStyle: 'italic', color: 'white' }}>
                     {testimonial.company}
                   </Typography>
                 </Box>
